049232f1104e98783bb77a3eb6c8a4e9c415909e
Bulong <369608@mcpsmd.org>
2019-11-13 09:11:18 -0500

    else if identifying children and teenagers

--- hello.py
+++ hello.py
@@ -3,4 +3,8 @@
 name = input("what is ur name")
 age = int(input("How old are you"))
 print("Hello" , name)
+if age>= 13 and age <19:
+    print("u are a teen")
+elif age < 13:
+    print("ur child")
 
