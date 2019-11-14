45e4b58ea490b5c25e1ecae7103643bff3c0c3da
Bulong <369608@mcpsmd.org>
2019-11-14 08:43:30 -0500

    create code that executes greeting from location

--- hello.py
+++ hello.py
@@ -1,10 +1,6 @@
 # Author : Michael-Bryant Bulong
 # Hello World Program
 name = input("what is ur name")
-age = int(input("How old are you"))
-print("Hello" , name)
-if age>= 13 and age <19:
-    print("u are a teen")
-elif age < 13:
-    print("ur child")
+town = input("where do u live")
+print ("hello, " + name + " from" + town + "!")
 
