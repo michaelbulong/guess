fcbed1c2312356e5dfe745da3637a3db8ae8cbf9
Bulong <369608@mcpsmd.org>
2019-11-19 08:58:03 -0500

    you have 10 chances to guess

--- guessinggame.py
+++ guessinggame.py
@@ -4,3 +4,8 @@ guessesTaken = 0
 
 number = random.randint(1, 25)
 print( "alright bro, im thinking of a number 1 to 25")
+
+while guessesTaken < 10
+    print ("take a guess")
+    guess = input()
+    guess = int(guess)
