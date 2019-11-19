52d1e6eeaf9c3c1c5d0ebd20811e46f2135d6f48
Bulong <369608@mcpsmd.org>
2019-11-19 08:47:07 -0500

    import random

--- hello.py
+++ hello.py
@@ -1,6 +1,11 @@
 # Author : Michael-Bryant Bulong
 # Hello World Program
 name = input("what is ur name")
+age = int(input("how old are you?"))
 town = input("where do u live")
 print ("hello, " + name + " from" + town + "!")
+if age >= 143 and age <= 19:
+    print("you are a teenager")
+elif age < 13:
+    print("ur a child")
 
